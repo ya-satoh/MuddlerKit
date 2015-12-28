caa51f2902846ec4a8055afba15b38c1a09b6fb9
matsuda <kosukematsuda@gmail.com>
2015-12-28 16:34:39 +0900

    create project

---
+++ MuddlerKit/MuddlerKit.h
@@ -0,0 +1,19 @@
+//
+//  MuddlerKit.h
+//  MuddlerKit
+//
+//  Created by Kosuke Matsuda on 2015/12/28.
+//  Copyright © 2015年 Kosuke Matsuda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MuddlerKit.
+FOUNDATION_EXPORT double MuddlerKitVersionNumber;
+
+//! Project version string for MuddlerKit.
+FOUNDATION_EXPORT const unsigned char MuddlerKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MuddlerKit/PublicHeader.h>
+
+
